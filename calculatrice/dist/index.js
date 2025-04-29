cde63af29490697dc0529cedf02699f51fd29898
DjibrilMTRAORE <traodjibril.250@gmail.com>
2025-04-29 12:28:49 +0000

    Les types

--- calculatrice/dist/index.js
+++ calculatrice/dist/index.js
@@ -3,3 +3,22 @@ function direBonjour(name) {
     return `Bonjour, ${name}!`;
 }
 console.log(direBonjour("Bou gatt"));
+let a = 5;
+a = 10;
+a = "bou gatt";
+a = false;
+let bailas = [5, 10, "bou gatt", false];
+let biranes = [5, 10, "bou gatt", false];
+bailas.push(55);
+let b = undefined;
+let c = null;
+let dataUnknown = ["bou gatt", 5, true, null, undefined];
+let dataAny = ["bou gatt", 5, true, null, undefined];
+dataAny.push(78);
+if (dataUnknown instanceof Array) {
+    dataUnknown.push(78);
+}
+let e = dataAny;
+if (typeof dataUnknown === "number") {
+    let f = dataUnknown;
+}
--- calculatrice/src/index.ts
+++ calculatrice/src/index.ts
@@ -1,8 +1,42 @@
-/*function direBonjour(name: string): string {
+/*
+function direBonjour(name: string): string {
     return `Bonjour, ${name}!`;
-   }*/
+}
+*/
 
-function direBonjour(name) {
+function direBonjour(name: any): string {
     return `Bonjour, ${name}!`;
 }
 console.log(direBonjour("Bou gatt"));
+
+type baila = number | string | boolean
+type numberNullable = number | null
+
+let a: baila = 5;
+a = 10
+a = "bou gatt"
+a = false
+
+let bailas: baila[] = [5, 10, "bou gatt", false];
+let biranes: (number | string | boolean)[] = [5, 10, "bou gatt", false];
+
+bailas.push(55)
+
+let b: number | undefined = undefined
+let c: numberNullable = null;
+
+let dataUnknown: unknown = ["bou gatt", 5, true, null, undefined]
+let dataAny: any = ["bou gatt", 5, true, null, undefined]
+
+//dataAny = 5
+dataAny.push(78);
+
+if(dataUnknown instanceof Array){
+    dataUnknown.push(78);
+}
+
+let e: number = dataAny;
+
+if(typeof dataUnknown === "number"){
+    let f: number = dataUnknown;
+}
